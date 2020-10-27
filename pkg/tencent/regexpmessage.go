ff9d0c0468625ebf6d0e1965637e1dc874c6e47c
lanbing222 <lanbing222zhu@126.com>
2020-10-27 17:18:36 +0800

    message  regexp function

--- pkg/tencent/regexpmessage.go
+++ pkg/tencent/regexpmessage.go
@@ -24,25 +24,22 @@ import (
 	"regexp"
 )
 
-func StrRegexp(SrcStr string,RegexpStr []string) {
-	for _,str:=range RegexpStr {
-		re:=str+"[-,a-z,0-9]*"
+func StrRegexp(SrcStr string, RegexpStr []string) string {
+	for _, str := range RegexpStr {
+		re := str + "[-,a-z,0-9]*"
 		restr := regexp.MustCompile(re)
-		FindStr :=restr.FindAll([]byte(SrcStr), -1)
-		if len(FindStr)!=0{
-			for k,i :=range(FindStr){
-				fmt.Println(k,string(i),"\n")
-			}
+		FindStr := restr.FindAll([]byte(SrcStr), -1)
+		if len(FindStr) != 0 {
+			return string(FindStr[0])
 		}
-		//if len(FindStr)!=0{
-		//	return
-		//}
+		//fmt.Println("1")
 	}
+	return ""
 }
 
-func main(){
-	RegexpStr:=[]string{"crm","ssm","dlm","wx"}
-	SrcStr:="User in User to {GET}/interfaces/server/app/queryOrderInfo/app006/interAspect in crm-dmscloud-customer-service-prd1"
-	StrRegexp(SrcStr,RegexpStr)
-	//fmt.Println(str)
+func main() {
+	RegexpStr := []string{"crm-", "ssm-", "dlm-", "wx-"}
+	SrcStr := "User in User to {GET}/interfaces/server/app/queryOrderInfo/app006/interAspect in wx-dmscloud-customer-service-prd1"
+	str := StrRegexp(SrcStr, RegexpStr)
+	fmt.Println(str)
 }
